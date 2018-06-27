62e4a9385a1a894486c229c272c913a841ec5c41
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-26 18:01:19 -0600

    Creates foods index api spec

--- spec/requests/api/v1/get_foods_spec.rb
+++ spec/requests/api/v1/get_foods_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 describe 'food API' do
   it 'returns JSON of all foods in the db' do
-    create(:food)
+    create_list(:food, 2)
+
+    get '/api/v1/foods'
+
+    foods = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(foods.count).to eq(2)
   end
 end
