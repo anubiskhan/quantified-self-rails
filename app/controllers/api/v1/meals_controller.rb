18e05f4d92906294cd3a9016abf55c77ce1ad7e0
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-27 23:43:38 -0600

    Adds show method and logic to meals controller

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -2,4 +2,9 @@ class Api::V1::MealsController < ApplicationController
   def index
     render json: Meal.all
   end
+
+  def show
+    meal = Meal.find_by_id(params[:meal_id])
+    meal ? (render json: meal) : (render json: {}, status: 404)
+  end
 end
