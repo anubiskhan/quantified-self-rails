5464fae64ead317adea02242f48ea1b0f88ae028
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-27 21:59:54 -0600

    Adds spec for updating food

---
+++ spec/requests/api/v1/update_food_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'food API' do
+  it 'can update a food entry' do
+    food = create(:food)
+    updated_food = { "food": { "name": "Garlic", "calories": 8} }
+
+    patch "/api/v1/foods", params: new_food
+
+    food_json = JSON.parse(response.body)
+    expected_json = {
+      id: 1,
+      name: 'Garlic',
+      calories: 8
+    }
+
+    expect(response).to be_success
+    expect(food_json.to_json).to eq(expected_json.to_json)
+  end
+  it 'returns 404 status if food fails to update' do
+    new_food = { "food": { "name": "Garlic"} }
+    patch "/api/v1/foods", params: new_food
+
+    expect(response).to_not be_success
+    expect(response.status).to eq(400)
+  end
+end
