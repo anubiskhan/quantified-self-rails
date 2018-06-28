ebed89ef7f2f79c21d9fe3cd945824e619ce3bff
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-27 23:11:31 -0600

    Adds relationship logic to meal, food, and mealfood models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,7 @@
 class Food < ApplicationRecord
   validates_presence_of :name, :calories
   validates :name, uniqueness: true
+
+  has_many :meal_foods, dependent: :destroy
+  has_many :meals, through: :meal_foods
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,7 @@
 class Meal < ApplicationRecord
   validates_presence_of :name
   validates :name, uniqueness: true
+
+  has_many :meal_foods, dependent: :destroy
+  has_many :foods, through: :meal_foods
 end
