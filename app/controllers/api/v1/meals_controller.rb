a57dd2efcf33e1181e5e87534e4d1a08883fa049
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-28 00:09:08 -0600

    Adds create logic to meals controller

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -7,4 +7,10 @@ class Api::V1::MealsController < ApplicationController
     meal = Meal.find_by_id(params[:meal_id])
     meal ? (render json: meal) : (render json: {}, status: 404)
   end
+
+  def create
+    meal = Meal.find_by_id(params[:meal_id])
+    food = Food.find_by_id(params[:id])
+    meal && food ? (meal.foods << food; render json: { message: "Successfully added #{food.name} to #{meal.name}" }) : (render json: {}, status: 404)
+  end
 end
