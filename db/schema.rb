00273ba2d5abee7f785fa3a67bfb1109db6269ef
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-27 22:53:07 -0600

    Adds migration for meals and migrates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_27_000632) do
+ActiveRecord::Schema.define(version: 2018_06_28_045051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2018_06_27_000632) do
     t.integer "calories"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "meals", force: :cascade do |t|
+    t.string "name"
   end
 
 end
