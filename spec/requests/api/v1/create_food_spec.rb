0f5d1a3f99188daef4bbe3be72ac5cfa80d8c8f2
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-27 21:06:03 -0600

    Improves spec, checking Food.all.count

--- spec/requests/api/v1/create_food_spec.rb
+++ spec/requests/api/v1/create_food_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe 'food API' do
   it 'can create a food entry' do
     new_food = { "food": { "name": "Garlic", "calories": 8} }
+
+    expect(Food.all.count).to eq(0)
+
     post "/api/v1/foods", params: new_food
 
     food_json = JSON.parse(response.body)
@@ -14,6 +17,7 @@ describe 'food API' do
 
     expect(response).to be_success
     expect(food_json.to_json).to eq(expected_json.to_json)
+    expect(Food.all.count).to eq(1)
   end
   it 'returns 404 status if food fails to create' do
     new_food = { "food": { "name": "Garlic"} }
