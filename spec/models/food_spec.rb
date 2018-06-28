3843b58e6ac4f08dc0607fc70b1da7fee06be9c6
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-27 23:08:44 -0600

    Adds model tests for relationships

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -5,4 +5,8 @@ describe Food, type: :model do
     it{should validate_presence_of(:name)}
     it{should validate_presence_of(:calories)}
   end
+  context 'relationships' do
+    it{should have_many(:meal_foods)}
+    it{should have_many(:meals)}
+  end
 end
